0676120e2e70eefa72666daf01380dca43870fe2
edveloso <edveloso@gmail.com>
2012-09-20 21:10:11 -0300

    inclusão de espanhol

--- sample-jsf-project/src/controle/Internacionalizacao.java
+++ sample-jsf-project/src/controle/Internacionalizacao.java
@@ -7,16 +7,19 @@ import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 
-
 @SessionScoped
 @ManagedBean(name="i18n")
 public class Internacionalizacao {
   
 	public void mudaPais(){
-		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-        String newLocale = externalContext.getRequestParameterMap().get("locale");
+ExternalContext externalContext = 
+		FacesContext.getCurrentInstance().getExternalContext();
+String newLocale = 
+        externalContext.getRequestParameterMap().get("locale");
+
         Locale locale = new Locale(newLocale);
-		FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+        
+FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
 	}
 	
 	
